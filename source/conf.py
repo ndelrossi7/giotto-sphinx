312ebd94f75cdf6943c56e5a6f33b691ac0f8954
Adriana Sistig <59894711+sistia01@users.noreply.github.com>
2021-08-03 17:44:47 -0500

    Add files via upload

---
+++ source/conf.py
@@ -0,0 +1,78 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Giotto'
+copyright = '2021, Ruben Dries, Qian Zhu, Huipeng Li, Rui Dong, Guo-Cheng Yuan'
+author = 'Developed by Ruben Dries, Qian Zhu, Huipeng Li, Rui Dong, Guo-Cheng Yuan.'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+import sphinx_rtd_theme
+
+extensions = ['sphinx.ext.autosectionlabel', 
+'sphinxcontrib.images', 
+'sphinx.ext.autodoc',
+'sphinx_autodoc_typehints', 
+'sphinx_panels', 
+'sphinx_togglebutton',
+'sphinx_rtd_theme' 
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+
+html_theme = "sphinx_rtd_theme"
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_logo = "images/GiottoLogo.png"
+
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+    'navigation_depth': 4,
+    #Toc Tree Options
+    'collapse_navigation': True,
+    #'sticky_navigation': True,
+}
+
+html_sidebars = { '**': ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'] }
+
+# Toc Tree options
+navtree_root_links = True
+navtree_shift = True
---
+++ source/index.rst
@@ -0,0 +1,51 @@
+.. Giotto documentation master file, created by
+   sphinx-quickstart on Fri May 14 14:03:07 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+################################
+Welcome to the Giotto website!
+################################
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Introduction 
+   :hidden:
+
+   general.rst
+   How to Get Started With Giotto <gettingstarted.rst> 
+   Giotto Workflow <giottoworkflowanalyses.rst>
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Giotto Example Datasets
+   :hidden:
+
+   datasets.rst   
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: How Tos and Tutorials
+   :hidden:
+
+   Working with Giotto Class <subsections/tipsandtricks/giottoclass.rst>
+   How to Subset Giotto <subsections/tipsandtricks/howtosubsetgiottoobject.rst>
+   How to Visualize Giotto Object <subsections/tipsandtricks/howtovisualizeandsaveplots.rst>
+   How to Test and Store Multiple <subsections/tipsandtricks/testandstoremultipleanayses.rst>
+   Ways to Visualize <subsections/tipsandtricks/waystovisualizespatialdata.rst>
+   Working with Images <subsections/tipsandtricks/workingwithimages.rst>
+   Visulize Voronoi <subsections/tipsandtricks/visualizewithvoronoiplots.rst>
+ 
+.. toctree::
+   :maxdepth: 1
+   :caption: Supplemental Information
+   :hidden:
+   
+   Errors and FAQS <errorsfaqsandtips.rst>
+   Additional Information <additionalinformation.rst>
+   Browse Source Code <https://​github.com/​RubD/​Giotto/​>
+   Report a Bug <https://​github.com/​RubD/​Giotto/​issues>
+
+
+
